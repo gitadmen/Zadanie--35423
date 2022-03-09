6a65ae41d2dc4b283986f286ce43b4e07c997e94
gitadmen <94184870+gitadmen@users.noreply.github.com>
2022-03-09 12:26:33 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-$(document).ready(function() {
+let btn = document.getElementById('get-data')
+
+let httpReq = new XMLHttpRequest();
+let dataNet;
+
+function getData() {
 
 
-});
+    console.log('getData has start')
+
+    httpReq.open('GET', 'https://akademia108.pl/api/ajax/get-users.php')
+
+    httpReq.onreadystatechange = () => {
+
+
+        dataNet = httpReq.responseText;
+        console.log(data)
+        console.log('Łączenie')
+
+
+
+
+    }
+}
+
+
+function scrollToEndOfPage() {
+
+    let scrHeight = document.documentElement.scrollHeight;
+    let scrTop = document.documentElement.scrollTop;
+    let clientHeight = document.documentElement.clientHeight;
+
+    if ((clientHeight + scrTop + 5) > scrHeight) {
+        getData();
+    }
+}
+
+
+window.addEventListener('scroll', scrollToEndOfPage)
